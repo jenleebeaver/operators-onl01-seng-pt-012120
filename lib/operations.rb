bdf48e394e24b67d7c7dd31bef5b0ac21fca7ec4
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-01-22 23:41:45 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -2,13 +2,11 @@
 #require "pry"
 
 def unsafe?(speed)
-	if speed > 60
-		true
-	elsif speed < 40
-		true
-	else
-		false
-	end
+	if speed > 60 || speed < 40
+		return true
+  end 
+	# default to false
+		return false
 end
 
 
