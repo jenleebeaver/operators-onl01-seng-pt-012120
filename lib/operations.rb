68646a43ceba24a9cbccc34839165f53533f4f3e
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-01-22 23:39:08 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,5 +1,5 @@
 
-require "pry"
+#require "pry"
 
 def unsafe?(speed)
 	if speed > 60
@@ -14,7 +14,7 @@ end
 
 
 def not_safe?(speed)
-	binding.pry
+	#binding.pry
 	# speed > 60 || speed < 40 ? true : false
 	speed.between?(40,60) ? false : true
 end
